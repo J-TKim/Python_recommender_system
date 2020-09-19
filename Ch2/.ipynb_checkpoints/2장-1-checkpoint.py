562b11e5a9dbf149a3504d0c44e879848a2fbe47
J-TKim <kkkd991020@naver.com>
2020-09-19 02:08:34 +0000

    예제 업로드

---
+++ Ch2/.ipynb_checkpoints/2장-1-checkpoint.py
@@ -0,0 +1,6 @@
+# u.user 파일을 DataFrame로 읽기
+import pandas as pd
+u_cols = ["user_id", "age", "sex", "occupation", "zip_code"]
+users = pd.read_csv("../data/u.user", sep="|", names=u_cols, encoding="latin-1")
+users = users.set_index("user_id")
+users.head()
