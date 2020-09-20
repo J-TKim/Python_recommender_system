f261ef8cf978d0de907511926d74086e1d1363b5
J-TKim <kkkd991020@naver.com>
2020-09-20 14:24:21 +0000

    2장-1 코드 업로드

--- Ch2/2장-1.py
+++ Ch2/2장-1.py
@@ -45,12 +45,43 @@ ratings.head()
 
 
 # Best-seller 추천
-def recom_moviel(n_items):
+def recom_movie1(n_items):
     movie_sort = movie_mean.sort_values(ascending=False)[:n_items]
     recom_movies = movies.loc[movie_sort.index]
     recommendations = recom_movies['title']
     return recommendations
 
 movie_mean = ratings.groupby(['movie_id'])['rating'].mean()
-recom_moviel(5)
+recom_movie1(5)
+
+
+# In[5]:
+
+
+def recom_moive2(n_items):
+    return movies.loc[movie_mean.sort_values(ascending=False)[:n_items].index]["title"]
+
+
+# ## 2.3 추천 시스템의 정확도 측정
+
+# In[6]:
+
+
+# 정확도 계산
+import numpy as np
+
+def RMSE(y_ture, y_pred):
+    return np.sqrt(np.mean((np.array(y_ture) - np.array(y_pred))**2))
+
+
+# In[7]:
+
+
+rmse = []
+for user in set(ratings.index):
+    y_true = ratings.loc[user]["rating"]
+    y_pred = movie_mean[ratings.loc[user]["movie_id"]]
+    accuracy = RMSE(y_true, y_pred)
+    rmse.append(accuracy)
+print(np.mean(rmse))
 
